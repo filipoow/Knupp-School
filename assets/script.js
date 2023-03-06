4c103f04e03b9fc8739893fb75ca23e09c018bb3
filipoow <emaildofilipe.m@gmail.com>
2023-03-06 20:06:10 -0300

    Creating Popup - Activities

--- assets/script.js
+++ assets/script.js
@@ -105,3 +105,10 @@ $("#matricula-button").click(function() {
       scrollTop: $("#form").offset().top},
       'slow');
 });
+
+function newPopup(){
+  varWindow = window.open (
+  'popup.html',
+  'pagina',
+  "width=350, height=255, top=100, left=110, scrollbars=no " );
+  }
